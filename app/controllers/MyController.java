c9c8ef2b57045bb97fcc69074fa4b9d1da9406e5
Farhan Laeeq <farhan.laeeq@publicissapient.com>
2019-04-03 15:46:58 +0530

    First Commit

---
+++ app/controllers/MyController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class MyController extends Controller {
+
+	public Result index() {
+		return ok("Hello Play from Eclipse Latest");
+	}
+
+	public Result views() {
+		return ok(views.html.palindrome.render());
+	}
+
+	public Result result() {
+		String request = request().getQueryString("Number");
+		int number = Integer.parseInt(request);
+		int temp = number;
+		int reverse = 0, rem;
+		while (number != 0) {
+			rem = number % 10;
+			reverse = reverse * 10 + rem;
+			number /= 10;
+
+		}
+
+		if (temp == reverse)
+			return ok(temp + " is a Palindrome Number ");
+		else
+			return ok(temp + " is not a Palindrome Number ");
+
+	}
+}
