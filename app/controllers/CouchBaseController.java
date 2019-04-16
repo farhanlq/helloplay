85ed157167f226abddd2e1806cc0f06d2d9b0037
Farhan Laeeq <farhan.laeeq@publicissapient.com>
2019-04-16 11:01:43 +0530

    Update CouchbaseController.java

--- app/controllers/CouchBaseController.java
+++ app/controllers/CouchBaseController.java
@@ -18,15 +18,22 @@ public class CouchBaseController extends Controller {
 
 	public Result employeeInfo(String id) {
 
+		// For openinig the connection
 		Cluster cluster = CouchbaseCluster.create("localhost:8091");
+		// For authentication
 		cluster.authenticate("admin", "admin123");
+		// Which bucket we want to open 
 		Bucket bucket = cluster.openBucket("employee");
+		// To retrieve the bucket
 		//JsonDocument doc = bucket.get(id);
+		
+		// Insert the data into the bucket
 		JsonObject user = JsonObject.empty().put("id", 6)
 				.put("name", "mohit")
 				.put("phone", "9882140698")
 				.put("dob", "07/07/1919");
 		
+		// Creation of id for the Document
 		JsonDocument doc = JsonDocument.create("emp_6", user);
 		JsonDocument response = bucket.upsert(doc);
 		
